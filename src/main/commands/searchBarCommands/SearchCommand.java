d4b0ab11a9e2118d5ef6a8b904f32235cd1cd338
victoriatrofim <victoriatrofim2002@gmail.com>
2023-11-24 17:01:52 +0200

    Added 2 classes for search and select command. Both contain methods and implementations for creating needed functionality

--- src/main/commands/searchBarCommands/SearchCommand.java
+++ src/main/commands/searchBarCommands/SearchCommand.java
@@ -4,37 +4,46 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.Data;
 import main.Main;
+import main.UserOptions.Database;
 import main.audioFile.Song;
 import main.audioFile.SongFilter;
-import main.colectionsAudio.Library;
+import main.colectionsAudio.*;
 import main.commands.CommandInterface;
 import main.test.Instruction;
-
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class SearchCommand implements CommandInterface {
-
+    private final int limit = 5;
     private SongFilter songFilter;
-    private Library library;
+    private LibraryPodcasts libraryPodcasts;
+    private PodcastFilter podcastFilter;
+    private LibrarySongs library;
     private ObjectMapper objectMapper;
     private Instruction instruction;
-    public SearchCommand() {
-        library = new Library();
+    private ArrayList<Playlist> playlistArrayList;
+    public SearchCommand(final Database database) {
+        library = new LibrarySongs();
         songFilter = new SongFilter(library.getSongs());
+        libraryPodcasts = new LibraryPodcasts();
+        podcastFilter = new PodcastFilter(libraryPodcasts.getPodcastList());
         objectMapper = new ObjectMapper();
     }
 
+    /**
+     * @param commandInstructions
+     */
     @Override
-    public void act(final Instruction instruction) {
-        this.instruction = instruction;
-        ActByType();
+    public void act(final Instruction commandInstructions) {
+        this.instruction = commandInstructions;
+        actByType();
     }
 
-    private void ActByType() {
+    private void actByType() {
         switch (instruction.getType()) {
             case "song": searchSong(); break;
+            case "playlist": searchPlaylist(); break;
             case "podcast": searchPodcast(); break;
             default: break;
         }
@@ -42,33 +51,124 @@ public class SearchCommand implements CommandInterface {
 
     private void searchSong() {
         if (instruction.getFilters() != null) {
+            List<Song> songList = library.getSongs();
             if (instruction.getFilters().getName() != null) {
-                List<Song> songList = songFilter.filterByName(library.getSongs(), instruction.getFilters().getName());
-                SongListToArrayNode(songList);
+                songList = songFilter.filterByName(
+                                songList, instruction.getFilters().getName());
             }
+            if (instruction.getFilters().getAlbum() != null) {
+                songList = songFilter.filterByAlbum(
+                        songList, instruction.getFilters().getAlbum());
+            }
+            if (instruction.getFilters().getTags() != null) {
+                songList = songFilter.filterByTags(
+                        songList, instruction.getFilters().getTags());
+            }
+            if (instruction.getFilters().getLyrics() != null) {
+                songList = songFilter.filterByLyrics(
+                        songList, instruction.getFilters().getLyrics());
+            }
+            if (instruction.getFilters().getGenre() != null) {
+                songList = songFilter.filterByGenre(
+                        songList, instruction.getFilters().getGenre());
+            }
+            if (instruction.getFilters().getReleaseYear() != null) {
+                songList = songFilter.filterByYear(
+                        songList, instruction.getFilters().getReleaseYear());
+            }
+            if (instruction.getFilters().getArtist() != null) {
+                songList = songFilter.filterByArtist(
+                        songList, instruction.getFilters().getArtist());
+            }
+            songListToArrayNode(songList);
+
         }
     }
 
     private void searchPodcast() {
-
+        if (instruction.getFilters() != null) {
+            List<Podcast> podcastList = libraryPodcasts.getPodcastList();
+            if (instruction.getFilters().getName() != null) {
+                podcastList = podcastFilter.filterByName(
+                        podcastList, instruction.getFilters().getName());
+            }
+            if (instruction.getFilters().getOwner() != null) {
+                podcastList = podcastFilter.filterByOwner(
+                        podcastList, instruction.getFilters().getOwner());
+            }
+            podcastListToArrayNode(podcastList);
+        }
     }
-
-    private void SongListToArrayNode(List<Song> songList) {
+    private void searchPlaylist() {
+        if (instruction.getFilters() != null) {
+            List<Playlist> playlistList = playlistArrayList;
+            if (instruction.getFilters().getName() != null) {
+                playlistList = PlaylistFilter.filterByName(
+                        playlistList, instruction.getFilters().getName());
+            }
+            if (instruction.getFilters().getOwner() != null) {
+                playlistList = PlaylistFilter.filterByOwner(
+                        playlistList, instruction.getFilters().getOwner());
+            }
+            playlistListToArrayNode(playlistList);
+        }
+    }
+    private void songListToArrayNode(final List<Song> songList) {
         ObjectNode objectNode = objectMapper.createObjectNode();
-        List<String> resultSongs = new ArrayList<>();
+        List<String> resultNameSongList = new ArrayList<>();
         songList.forEach(song -> {
-            if (resultSongs.size() < 5) {
-                resultSongs.add(song.getName());
+            if (resultNameSongList.size() < limit) {
+                Main.songListAfterSearch.add((song));
+                resultNameSongList.add(song.getName());
             }
         });
         objectNode.set("command", objectMapper.valueToTree(instruction.getCommand()));
         objectNode.set("user", objectMapper.valueToTree(instruction.getUsername()));
         objectNode.set("timestamp", objectMapper.valueToTree(instruction.getTimestamp()));
-        objectNode.set("message", objectMapper.valueToTree("Search returned " + resultSongs.size() + " results"));
-        objectNode.set("results", objectMapper.valueToTree(resultSongs));
+        objectNode.set("message",
+                objectMapper.valueToTree(
+                        "Search returned " + resultNameSongList.size() + " results"));
+        objectNode.set("results", objectMapper.valueToTree(resultNameSongList));
         Main.output.add(objectNode);
+
     }
 
+    private void podcastListToArrayNode(final List<Podcast> podcastList) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        List<String> resultNamePodcastList = new ArrayList<>();
+        podcastList.forEach(podcast -> {
+            if (resultNamePodcastList.size() < limit) {
+                Main.podcastListAfterSearch.add((podcast));
+                resultNamePodcastList.add(podcast.getName());
+            }
+        });
+        objectNode.set("command", objectMapper.valueToTree(instruction.getCommand()));
+        objectNode.set("user", objectMapper.valueToTree(instruction.getUsername()));
+        objectNode.set("timestamp", objectMapper.valueToTree(instruction.getTimestamp()));
+        objectNode.set("message",
+                objectMapper.valueToTree(
+                        "Search returned " + resultNamePodcastList.size() + " results"));
+        objectNode.set("results", objectMapper.valueToTree(resultNamePodcastList));
+        Main.output.add(objectNode);
 
+    }
+    private void playlistListToArrayNode(final List<Playlist> playlistList) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        List<String> resultNamePlaylistList = new ArrayList<>();
+        playlistList.forEach(playlist -> {
+            if (resultNamePlaylistList.size() < limit) {
+                Main.playlistListAfterSearch.add((playlist));
+                resultNamePlaylistList.add(playlist.getName());
+            }
+        });
+        objectNode.set("command", objectMapper.valueToTree(instruction.getCommand()));
+        objectNode.set("user", objectMapper.valueToTree(instruction.getUsername()));
+        objectNode.set("timestamp", objectMapper.valueToTree(instruction.getTimestamp()));
+        objectNode.set("message",
+                objectMapper.valueToTree(
+                        "Search returned " + resultNamePlaylistList.size() + " results"));
+        objectNode.set("results", objectMapper.valueToTree(resultNamePlaylistList));
+        Main.output.add(objectNode);
 
+    }
 }
--- src/main/commands/searchBarCommands/SelectCommand.java
+++ src/main/commands/searchBarCommands/SelectCommand.java
@@ -1,13 +1,138 @@
 package main.commands.searchBarCommands;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.Data;
+import main.Main;
+import main.UserOptions.Database;
+import main.audioFile.Song;
+import main.colectionsAudio.Playlist;
+import main.colectionsAudio.Podcast;
 import main.commands.CommandInterface;
 import main.test.Instruction;
+import java.util.ArrayList;
+@Data
+public final class SelectCommand implements CommandInterface {
+    private final int limit = 5;
+    private ObjectMapper objectMapper;
+    private static Song selectedSong;
+    private static Podcast selectedPodcast;
+    private static Playlist selectedPlaylist;
+    private static String isSelected;
 
-public class SelectCommand implements CommandInterface {
+    public static String getIsSelected() {
+        return isSelected;
+    }
 
+    public void setIsSelected(final String isSelected) {
+        this.isSelected = isSelected;
+    }
+
+    public static Song getSelectedSong() {
+        return selectedSong;
+    }
+
+    public static void setSelectedSong(final Song selectedSong) {
+        SelectCommand.selectedSong = selectedSong;
+    }
+
+    public static Podcast getSelectedPodcast() {
+        return selectedPodcast;
+    }
+
+    public static void setSelectedPodcast(final Podcast selectedPodcast) {
+        SelectCommand.selectedPodcast = selectedPodcast;
+    }
+
+    public static Playlist getSelectedPlaylist() {
+        return selectedPlaylist;
+    }
+
+    public static void setSelectedPlaylist(final Playlist selectedPlaylist) {
+        SelectCommand.selectedPlaylist = selectedPlaylist;
+    }
+
+    public SelectCommand(final Database database) {
+        objectMapper = new ObjectMapper();
+    }
     @Override
-    public void act(Instruction instruction) {
+    public void act(final Instruction instruction) {
+        if (!Main.songListAfterSearch.equals(new ArrayList<>())) {
+            selectSong(instruction);
+        } else if (!Main.podcastListAfterSearch.equals(new ArrayList<>())) {
+            selectPodcast(instruction);
+        } else if (!Main.playlistListAfterSearch.equals(new ArrayList<>())) {
+            selectPlaylist(instruction);
+        } else {
+            arrayNodeWithMessage(instruction,
+                    "Please conduct a search before making a selection.");
+        }
+    }
 
+    /**
+     * @param instruction
+     */
+    public void selectSong(final Instruction instruction) {
+        if (instruction.getItemNumber()
+                <= limit && instruction.getItemNumber()
+                <= Main.songListAfterSearch.size()) {
+            selectedSong = Main.songListAfterSearch.get(instruction.getItemNumber() - 1);
+            setIsSelected("song");
+            arrayNodeWithMessage(
+                    instruction,
+                    "Successfully selected " + selectedSong.getName() + ".");
+        } else {
+            arrayNodeWithMessage(instruction, "The selected ID is too high.");
+        }
+    }
+
+    /**
+     * @param instruction
+     */
+    public void selectPodcast(final Instruction instruction) {
+        if (instruction.getItemNumber()
+                <= limit && instruction.getItemNumber()
+                <= Main.podcastListAfterSearch.size()) {
+            selectedPodcast = Main.podcastListAfterSearch.get(instruction.getItemNumber() - 1);
+            setIsSelected("podcast");
+            arrayNodeWithMessage(
+                    instruction,
+                    "Successfully selected " + selectedPodcast.getName() + ".");
+        } else {
+            arrayNodeWithMessage(instruction, "The selected ID is too high.");
+        }
+    }
+
+    /**
+     * @param instruction
+     */
+    public void selectPlaylist(final Instruction instruction) {
+        if (instruction.getItemNumber()
+                <= limit && instruction.getItemNumber()
+                <= Main.playlistListAfterSearch.size()) {
+            selectedPlaylist = Main.playlistListAfterSearch.get(instruction.getItemNumber() - 1);
+            setIsSelected("playlist");
+            arrayNodeWithMessage(
+                    instruction,
+                    "Successfully selected " + selectedPlaylist.getName() + ".");
+        } else {
+            arrayNodeWithMessage(instruction, "The selected ID is too high.");
+        }
+    }
+
+    /**
+     * @param instruction
+     * @param message
+     */
+    private void arrayNodeWithMessage(final Instruction instruction, final String message) {
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        objectNode.set("command", objectMapper.valueToTree(instruction.getCommand()));
+        objectNode.set("user", objectMapper.valueToTree(instruction.getUsername()));
+        objectNode.set("timestamp", objectMapper.valueToTree(instruction.getTimestamp()));
+        objectNode.set("message", objectMapper.valueToTree(message));
+        Main.output.add(objectNode);
+        Main.songListAfterSearch = new ArrayList<>();
+        Main.podcastListAfterSearch = new ArrayList<>();
+        Main.playlistListAfterSearch = new ArrayList<>();
     }
 }
