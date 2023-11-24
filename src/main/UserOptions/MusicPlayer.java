9e538b04fa78bed4c1d69a73803e7e0b22b0ed59
victoriatrofim <victoriatrofim2002@gmail.com>
2023-11-24 17:09:33 +0200

    Added MusicPlayer class that will represent the music player of a user. I made MusicPlayer as a parent class for all the commands that have something in common with player, this way I manipulate the musicPlayer via classes that extends it

--- src/main/UserOptions/MusicPlayer.java
+++ src/main/UserOptions/MusicPlayer.java
@@ -1,44 +1,30 @@
 package main.UserOptions;
 
+import lombok.Data;
+import main.audioFile.PodcastEpisode;
 import main.audioFile.Song;
-
-class MusicPlayer {
+@Data
+public class MusicPlayer {
     private Song currentTrack;
-    private int currentPosition;
     private boolean isPlaying;
+    private PodcastEpisode lastPodcastEpisode;
+    private fileio.input.PodcastEpisode currentEpisode;
+    private int currentPosition;
 
-    MusicPlayer() {
-        this.currentTrack = null;
-        this.currentPosition = 0;
-        this.isPlaying = false;
+    /**
+     *
+     * @return
+     */
+    public boolean isPlaying() {
+        return isPlaying;
     }
 
-    public void play(final Song song) {
-        this.currentTrack = song;
-        this.currentPosition = 0;
-        this.isPlaying = true;
-        System.out.println("Playing: " + currentTrack + " at position: " + currentPosition);
-    }
-
-    public void pause() {
-        if (isPlaying) {
-            isPlaying = false;
-            System.out.println("Paused: " + currentTrack + " at position: " + currentPosition);
-        }
-    }
-
-    public void resume() {
-        if (!isPlaying) {
-            isPlaying = true;
-            System.out.println("Resumed: " + currentTrack + " from position: " + currentPosition);
-        }
-    }
-
-    public void progress(final int seconds) {
-        if (isPlaying) {
-            currentPosition += seconds;
-            System.out.println("Current position: " + currentPosition);
-        }
+    /**
+     *
+     * @param playing
+     */
+    public void setPlaying(final boolean playing) {
+        isPlaying = playing;
     }
 }
 
